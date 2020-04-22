f10e85cd108ef14fbc8fdede4f94b30e80ba5d9a
Paulo-Sergio <paulofranca.contato@gmail.com>
2020-04-22 12:00:09 -0300

    construção do dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Page;
+use App\User;
+use App\Visitor;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,8 +16,51 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        return view('admin.home');
+        $visitsCount = 0;
+        $onlineCount = 0;
+        $pagesCount = 0;
+        $usersCount = 0;
+        $interval = intval($request->input('interval', 30));
+        if ($interval > 120) {
+            $interval = 120;
+        }
+
+        //contagem de visitantes
+        $dateInterval = date('Y-m-d H:i:s', strtotime('-'.$interval.' days'));
+        $visitsCount = Visitor::where('date_access', '>=', $dateInterval)
+            ->count();
+
+        //contagem de paginas
+        $pagesCount = Page::count();
+
+        //contagem de usuarios
+        $usersCount = User::count();
+
+        //contagem de usuarios online
+        $datelimit = date('Y-m-d H:i:s', strtotime('-5 minutes'));
+        $onlineList = Visitor::select('ip')
+            ->where('date_access', '>=', $datelimit)
+            ->groupBy('ip')
+            ->get();
+        $onlineCount = count($onlineList);
+
+        //contagem para o PagePie
+        $pagePie = [];
+        $visitsAll = Visitor::selectRaw('page, count(page) as c')
+            ->where('date_access', '>=', $dateInterval)
+            ->groupBy('page')
+            ->get();
+
+        foreach ($visitsAll as $visit) {
+            $pagePie[$visit['page']] = intval($visit['c']);
+        }
+
+        $pageLabels = json_encode(array_keys($pagePie));
+        $pageValues = json_encode(array_values($pagePie));
+
+        return view('admin.home', compact('visitsCount', 'onlineCount', 'pagesCount', 'usersCount', 
+            'pageLabels', 'pageValues', 'interval'));
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,11 +1,125 @@
 @extends('adminlte::page')
 
-@section('title', 'Painel')
+@section('plugins.Chartjs', true)
+
+@section('title', 'Dashboard')
 
 @section('content_header')
-	<h1>Painel de controle</h1>
+	<div class="row">
+		<div class="col-md-6">
+			<h1>Dashboard</h1>
+		</div>
+		<div class="col-md-6">
+			<form method="GET">
+				<select onchange="this.form.submit()" name="interval" id="interval" class="float-md-right">
+					<option {{$interval == 30 ? 'selected="selected"' : ''}} value="30">Últimos 30 dias</option>
+					<option {{$interval == 60 ? 'selected="selected"' : ''}} value="60">Últimos 60 dias</option>
+					<option {{$interval == 90 ? 'selected="selected"' : ''}} value="90">Últimos 90 dias</option>
+					<option {{$interval == 120 ? 'selected="selected"' : ''}} value="120">Últimos 120 dias</option>
+				</select>
+			</form>
+		</div>
+	</div>
 @endsection
 
 @section('content')
-	...
+	
+	<div class="row">
+		<div class="col-md-3">
+			<div class="small-box bg-info">
+				<div class="inner">
+					<h3>{{$visitsCount}}</h3>
+					<p>Acessos</p>
+				</div>
+				<div class="icon">
+					<i class="far fa-fw fa-eye"></i>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3">
+			<div class="small-box bg-success">
+				<div class="inner">
+					<h3>{{$onlineCount}}</h3>
+					<p>Usuários online</p>
+				</div>
+				<div class="icon">
+					<i class="far fa-fw fa-heart"></i>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3">
+			<div class="small-box bg-warning">
+				<div class="inner">
+					<h3>{{$pagesCount}}</h3>
+					<p>Páginas</p>
+				</div>
+				<div class="icon">
+					<i class="far fa-fw fa-sticky-note"></i>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3">
+			<div class="small-box bg-danger">
+				<div class="inner">
+					<h3>{{$usersCount}}</h3>
+					<p>Usuários</p>
+				</div>
+				<div class="icon">
+					<i class="far fa-fw fa-user"></i>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-6">
+			<div class="card">
+				<div class="card-header">
+					<h3 class="card-title">Páginas mais visitadas</h3>
+				</div>
+				<div class="card-body">
+					<canvas id="pagePie"></canvas>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-6">
+			<div class="card">
+				<div class="card-header">
+					<h3 class="card-title">Sobre o sistema</h3>
+				</div>
+				<div class="card-body">
+					...
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script>
+
+		window.onload = function () {
+			let ctx = document.getElementById('pagePie').getContext('2d');
+			window.pagePie = new Chart(ctx, {
+				type: 'pie',
+				data: {
+					datasets: [{
+						data: {{$pageValues}},
+						backgroundColor: '#0000FF'
+					}],
+					labels: {!!$pageLabels!!}
+				},
+				options: {
+					responsive: true,
+					legend: {
+						display: false
+					}
+				}
+			});
+		}
+
+	</script>
+
 @endsection
